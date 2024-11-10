092b97e76525dff65804e3c72179a538874bed44
dougreisss <douglas_rrs@hotmail.com>
2024-11-10 17:12:06 -0300

    :boom: fix: routes book

--- backend/routes/bookRoutes.js
+++ backend/routes/bookRoutes.js
@@ -7,7 +7,7 @@ router.get('/', bookController.getAllBooks);
 router.get('/id/:id', bookController.getBookById);
 router.get('/details', bookController.getAllWithDetails)
 router.post('/', bookController.createBook);
-router.put('/:id', bookController.updateBook);
-router.delete('/:id', bookController.deleteBook);
+router.put('/id/:id', bookController.updateBook);
+router.delete('/id/:id', bookController.deleteBook);
 
 module.exports = router;
