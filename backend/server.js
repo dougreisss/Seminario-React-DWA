ef4a26c468a0a4a60dd9ed2462e3e5e4037554cd
Douglas Reis <59988180+dougreisss@users.noreply.github.com>
2024-11-12 23:17:41 +0000

    :sparkles: feat: css styles book

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,13 @@ const PORT = process.env.PORT || 5000;
 const bookRoutes = require('./routes/bookRoutes');
 
 app.use(cors());
+
+app.use(cors({
+    origin: '*', 
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
+    allowedHeaders: ['Content-Type', 'Authorization'], 
+}));
+  
 app.use(express.json());
 
 app.use('/api/book', bookRoutes);
--- frontend/src/components /Books/BookList.js
+++ frontend/src/components /Books/BookList.js
@@ -1,6 +1,7 @@
-import '../App.css';
+import styles from './BookList.module.css';
 import React, { useState, useEffect } from 'react';
-import { getBooks } from '../api';
+import { getBooks } from '../../api';
+import { format } from 'date-fns';
 
 function BookList() {
 
@@ -16,15 +17,19 @@ function BookList() {
         fetchBooks();
     }, []);
 
+    const formatDate = (dateString) => {
+        return format(new Date(dateString), 'dd/MM/yyyy');
+    };
+
     return (
-        <div>
-            <h2>Lista de Livros</h2>
-            <table>
+        <div className={styles.container}>
+            <h2 className={styles.title}>Lista de Livros</h2>
+            <table className={styles.table}>
                 <thead>
                     <tr>
                         <th>Título</th>
                         <th>Sinopse</th>
-                        <th>Data de publicação</th>
+                        <th>Data de Publicação</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -32,7 +37,7 @@ function BookList() {
                         <tr key={book.book_id}>
                             <td>{book.title}</td>
                             <td>{book.synopsis}</td>
-                            <td>{book.publication_date}</td>
+                            <td>{formatDate(book.publication_date)}</td>
                         </tr>
                     ))}
                 </tbody>
