9aa8deab7692e8af9c7a1d9c39d95e7ac668dcdb
dougreisss <douglas_rrs@hotmail.com>
2024-12-12 19:05:33 -0300

    :boom: fix: rename title - componentes 'UpdateReviewForm'

--- frontend/src/components/Review/UpdateReviewForm.js
+++ frontend/src/components/Review/UpdateReviewForm.js
@@ -70,7 +70,7 @@ function UpdateReviewForm () {
 
     return (
         <div className="max-w-3xl mx-auto p-4">
-            <h2 className="text-3xl font-bold text-center text-gray-800 mb-8">Editar livro</h2>
+            <h2 className="text-3xl font-bold text-center text-gray-800 mb-8">Editar Review</h2>
 
             <button
                 className="bg-gray-500 text-white px-4 py-2 rounded hover:bg-gray-600 transition mb-4"
